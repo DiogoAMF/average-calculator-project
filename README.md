46082d7cf4dac6afd6e3bed1f10bd5458a23beff
Diogo Fernandes <diogof.sep@hotmail.com>
2024-03-21 19:50:24 -0300

    commit inital

---
+++ README.md
@@ -0,0 +1,36 @@
+<h1 align="center"> Calculadora de Média </h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="projeto calculadora" src="https://i.imgur.com/DuaSSJm.png">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="projeto calculadora" src="https://i.imgur.com/shso1pK.png">
+</p>
+
+## 🛠️ Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+
+## 💻 Projeto
+
+Uma calculadora de média é uma ferramenta que permite calcular a média de um conjunto de notas escolares. Geralmente, as notas são atribuídas em uma escala numérica, como de 0 a 10, e representam o desempenho de um aluno em diferentes disciplinas ou atividades.
---
+++ main.js
@@ -0,0 +1,84 @@
+const form = document.getElementById("form-atividade");
+const imgAprovado = '<img src="./image/aprovado.png" alt="Emoji Feliz">';
+const imgReprovado = '<img src="./image/reprovado.png" alt="Emoji Triste">';
+const atividades = [];
+const notas = [];
+const spanAprovado = `<span class="resultado aprovado">Aprovado</span>`;
+const spanReprovado = `<span class="resultado reprovado">Reprovado</span>`;
+
+const notaMinima = parseFloat(prompt("Digite a nota minima:"));
+
+let linhas = " "; // Inicializando a variável linhas
+
+// Adicionando um ouvinte de evento para o envio do formulário
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevenindo o comportamento padrão do formulário
+
+  adicionaLinha(); // Chamando a função para adicionar uma linha à tabela
+  atualizaTabela(); // Chamando a função para atualizar a tabela
+  atualizaMediaFinal(); // Chamando a função para atualizar a média final
+});
+
+// Função para adicionar uma linha à tabela
+function adicionaLinha() {
+  // Obtendo os elementos de input do nome e da nota da atividade
+  const inputNomeAtividade = document.getElementById("nome-atividade");
+  const inputNotaAtividade = document.getElementById("nota-atividade");
+
+  // Verificando se a atividade já foi inserida
+  if (atividades.includes(inputNomeAtividade.value)) {
+    alert(`A atividade ${inputNomeAtividade.value} já foi inserida`);
+  } else {
+    // Adicionando o nome da atividade e sua nota aos arrays correspondentes
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
+
+    // Criando uma linha HTML para a tabela
+    let linha = "<tr>";
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${
+      inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado
+    }</td>`;
+    linha += `</tr>`;
+
+    // Adicionando a linha à string linhas
+    linhas += linha; /* += é o operador de concatenação */
+  }
+
+  // Resetando os campos de input
+  inputNomeAtividade.value = " ";
+  inputNotaAtividade.value = " ";
+}
+
+// Função para atualizar a tabela na página HTML
+function atualizaTabela() {
+  const corpoTabela = document.querySelector("tbody"); // Selecionando o corpo da tabela
+  corpoTabela.innerHTML = linhas; // Atualizando o conteúdo do corpo da tabela com as linhas
+}
+
+// Função para atualizar a média final na página HTML
+function atualizaMediaFinal() {
+  // Calculando a média final das notas
+  const mediaFinal = calculaMediaFinal();
+
+  // Atualizando o valor da média final na página HTML
+  document.getElementById("media-final-valor").innerHTML = mediaFinal;
+
+  // Determinando se o aluno foi aprovado ou reprovado e atualizando o resultado na página HTML
+  document.getElementById("media-final-resultado").innerHTML =
+    mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+}
+
+// Função para calcular a média final das notas
+function calculaMediaFinal() {
+  let somaDasNotas = 0; // Variável para armazenar a soma das notas
+
+  // Loop para percorrer todas as notas e calcular a soma
+  for (let i = 0; i < notas.length; i++) {
+    somaDasNotas += notas[i]; // Adicionando o valor da nota à soma
+  }
+
+  // Calculando e retornando a média final
+  return somaDasNotas / notas.length;
+}
